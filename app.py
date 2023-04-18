ec803deff9efb968c044863c3494ba086d13f226
Parneet1996 <131126173+Parneet1996@users.noreply.github.com>
2023-04-18 10:19:11 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+
+st.write("""
+# Greater out of three numbers
+""")
+#Get Input
+st.header('User Input Parameters')
+def user_input_features():
+	num1=st.number_input("NUM1")
+	num2=st.number_input("NUM2")
+	num3=st.number_input("NUM3")
+
+	data={'NUM1':num1,'NUM2':num2,'NUM3':num3}
+	features = pd.DataFrame(data, index=[0])
+	return features
+df = user_input_features()
+st.write("Result")
+#a=features.loc[0,'NUM1']
+#st.write(df.iloc[0,0])
+if df.iloc[0,0]>df.iloc[0,1] and df.iloc[0,0]>df.iloc[0,2]:
+	st.write(df.iloc[0,0],"is greatest")
+elif df.iloc[0,1]>df.iloc[0,0] and df.iloc[0,1]>df.iloc[0,2]:
+	st.write(df.iloc[0,1],"is greatest" )
+else:
+	st.write(df.iloc[0,2],"is greatest")
+	
+
+
+
